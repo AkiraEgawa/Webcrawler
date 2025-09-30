eda4b2df5257ce57b17f76f496a4c04313247691
Akira Egawa <lucian.ming.chen@gmail.com>
2025-09-30 16:31:36 -0700

    made file parser

---
+++ lib/fileParser.py
@@ -0,0 +1,33 @@
+# This will take a file as instructions and return them as an array of information
+
+# File structure wanted
+"""
+First line: Starting URL
+Second line: Depth
+Third Line: URL Output file
+Fourth Line: Tag/text Output file
+Fifth Line onwards: Desired tags to extract
+"""
+
+def fileParser(file):
+    answer = []
+    tags = []
+    with open(file) as f:
+        lines = [line.strip() for line in f]
+        if (len(lines) < 4):
+            print("Instruction file too short, missing critical info")
+            return -1
+        if (len(lines) < 5):
+            print("Missing tags (if don't want tags just put in a)")
+            return -1
+        
+        # return instructions as dictionary
+        instructions = {
+            "start_url": lines[0],
+            "depth": int(lines[1]),
+            "url_output": lines[2],
+            "tag_output": lines[3],
+            "tags": lines[4:]
+        }
+    
+    return answer
